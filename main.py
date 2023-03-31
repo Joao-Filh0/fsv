f6e13f845f7957eb44bedeb843b2d324e904ab4a
Joao-Filh0 <joaoadmob2@gmail.com>
2023-03-31 18:00:54 -0300

    pyproject

--- main.py
+++ main.py
@@ -6,25 +6,31 @@ from src.manager_path import ManagePath
 from src.pub_get.pub_get import PubGet
 from src.rename_version import rename_version
 
-parser = argparse.ArgumentParser(description=description)
 
-parser.add_argument('--change', '-c', required=False)
-parser.add_argument('--list', '-l', required=False, const='l', nargs='?')
-parser.add_argument('--path', '-p', required=False)
-parser.add_argument('--pub-get', '-pg', required=False, const='desc', nargs='?')
+def main():
+    parser = argparse.ArgumentParser(description=description)
 
-args = parser.parse_args()
+    parser.add_argument('--change', '-c', required=False)
+    parser.add_argument('--list', '-l', required=False, const='l', nargs='?')
+    parser.add_argument('--path', '-p', required=False)
+    parser.add_argument('--pub-get', '-pg', required=False, const='desc', nargs='?')
 
-manage_path = ManagePath()
-get_path = manage_path.get_path()
-config_path = get_path if get_path else path_flutter
+    args = parser.parse_args()
 
-if vars(args).get('change'):
-    rename_version(arg=args.change, path_flutter=config_path, label=label, version_label=version_label)
-elif vars(args).get('list'):
-    list_version(path_flutter=config_path, version_label=version_label, label=label)
-elif vars(args).get('path'):
-    manage_path.save(args.path)
-elif vars(args).get('pub_get'):
-    pub_get = PubGet(args.pub_get)
-    pub_get.run()
+    manage_path = ManagePath()
+    get_path = manage_path.get_path()
+    config_path = get_path if get_path else path_flutter
+
+    if vars(args).get('change'):
+        rename_version(arg=args.change, path_flutter=config_path, label=label, version_label=version_label)
+    elif vars(args).get('list'):
+        list_version(path_flutter=config_path, version_label=version_label, label=label)
+    elif vars(args).get('path'):
+        manage_path.save(args.path)
+    elif vars(args).get('pub_get'):
+        pub_get = PubGet(args.pub_get)
+        pub_get.run()
+
+
+if __name__ == "__main__":
+    main()
