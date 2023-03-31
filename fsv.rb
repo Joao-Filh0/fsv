f636dd10c0f581853b699efcdce8c1e171e76635
Joao-Filh0 <joaoadmob2@gmail.com>
2023-03-31 19:32:59 -0300

    1.0.4-fix-permission-09

--- fsv.rb
+++ fsv.rb
@@ -1,10 +1,10 @@
 class Fsv < Formula
   include Language::Python::Virtualenv
 
-  url "https://github.com/Joao-Filh0/flutter_super_version/archive/refs/tags/0.0.4.tar.gz"
+  url "https://github.com/Joao-Filh0/fsv/archive/refs/tags/0.0.4.tar.gz"
   sha256 "add58bedc33350eec4f8cbe92ca4c6392d6980e054f10fdcc964589fcbf9717f"
   version "1.0.4"
-  head "https://github.com/Joao-Filh0/flutter_super_version.git", branch: "main"
+  head "https://github.com/Joao-Filh0/fsv.git", branch: "main"
 
   depends_on "python@3.10"
 
@@ -14,7 +14,7 @@ class Fsv < Formula
 
   (buildpath/"fsv").write <<~EOS
     #!/bin/bash
-    exec "#{libexec}/bin/python" "#{libexec}/lib/python3.10/site-packages/flutter_super_version/main.py" "$@"
+    exec "#{libexec}/bin/python" "#{libexec}/lib/python3.10/site-packages/fsv/main.py" "$@"
   EOS
 
   chmod 0755, buildpath/"fsv"
