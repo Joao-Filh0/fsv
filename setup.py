2b7b2304e5bb22c8e59e760ea896c2ba49398313
Joao-Filh0 <joaoadmob2@gmail.com>
2023-04-01 17:20:52 -0300

    up version 0.0.13

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="fsv",
-    version="0.0.12",
+    version="0.0.13",
     description="Utilitários para desenvolvedor Flutter",
     author="João Alves",
     author_email="militaof522@gmail.com",
     packages=find_packages(),
     package_dir={"": "."},
     install_requires=[
-        "argparse>=1.4.0",
         "requests",
+        "argparse>=1.4.0",
+
     ],
     entry_points={
         "console_scripts": [
