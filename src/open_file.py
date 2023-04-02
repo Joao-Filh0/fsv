c64fff943437b5aa75654f1ea3f76c398c3728b4
Joao-Filh0 <joaoadmob2@gmail.com>
2023-04-02 19:56:36 -0300

    colorama+3

--- src/open_file.py
+++ src/open_file.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+from colorama import Fore, Style
+
+
 def open_file(path: str, message='', is_print=True, check=''):
     with open(path, "r") as version:
         version = message + version.read()
         if is_print:
-            print(version+check)
+            if check:
+                color = Fore.GREEN
+            else:
+                color = Fore.YELLOW
+            print(color + version + check + Style.RESET_ALL)
         return version
