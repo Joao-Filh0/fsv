663040bf2e9c792ca43528b985f4ccccab9132b1
joao <joaoadmob2@gmail.com>
2023-04-03 00:10:56 -0300

    delete win+1

--- src/helpers/remove_folder.py
+++ src/helpers/remove_folder.py
@@ -7,12 +7,12 @@ def remove_folder(dir_path):
     for item in os.listdir(dir_path):
         item_path = os.path.join(dir_path, item)
 
-        os.chmod(item_path, 0o700)
         if os.path.isfile(item_path):
             os.unlink(item_path)
         else:
             is_win = is_win()
             if is_win:
+                os.chmod(item_path, 0o700)
                 os.system(f'rmdir /s /q "{item_path}"')
             else:
                 shutil.rmtree(item_path)
