1f9d2b8b369ff813347c02cd5ac73449b7a448af
Joao-Filh0 <joaoadmob2@gmail.com>
2023-04-03 07:48:01 -0300

    fix-bug

--- src/rename_version.py
+++ src/rename_version.py
@@ -6,14 +6,18 @@ import os
 
 
 def rename_version(arg: str, path_flutter: str, label: str, version_label: str):
-    flutter_current_version_name = open_file(os.path.join(path_flutter, label, version_label), is_print=False)
+    os.chdir(path_flutter)
+    try:
+        flutter_current_version_name = open_file(os.path.join(path_flutter, label, version_label), is_print=False)
+        os.rename(label, flutter_current_version_name)
+    except Exception as e:
+        pass
     versions = list_version(path_flutter=path_flutter, version_label=version_label, label=label, is_print=False)
     if arg not in versions:
         print('Flutter version not found')
         return
-    os.chdir(path_flutter)
+
     try:
-        os.rename(label, flutter_current_version_name)
         os.rename(arg, label)
         print(f'The current version is {arg}')
     except:
