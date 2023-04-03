4e031b6ae9c370c579bcdd3bda8c40ed90014ed2
Joao-Filh0 <joaoadmob2@gmail.com>
2023-04-03 08:48:04 -0300

    fix-bug+6

--- src/get_folder_sizes/get_folder_size.py
+++ src/get_folder_sizes/get_folder_size.py
@@ -3,30 +3,38 @@ import os
 
 
 class GetFolderSize:
+    import os
 
-    def __get_size(self, path):
-        total_size = 0
-        for dirpath, dirnames, filenames in os.walk(path):
-            for file in filenames:
-                filepath = os.path.join(dirpath, file)
-                total_size += os.path.getsize(filepath)
-        return total_size
+    def _convert_size(self, size_bytes: int) -> str:
+        try:
+            if size_bytes < 0:
+                raise ValueError("Error")
+            elif size_bytes >= 2 ** 30:
+                size_in_gb = size_bytes / 2 ** 30
+                return f"{round(size_in_gb, 2)} GB"
+            elif size_bytes >= 2 ** 20:
+                size_in_mb = size_bytes / 2 ** 20
+                return f"{round(size_in_mb, 2)} MB"
+            elif size_bytes >= 2 ** 10 or size_bytes == 1:
+                size_in_kb = size_bytes / 2 ** 10
+                return f"{round(size_in_kb, 2)} KB"
+            else:
+                return f"{size_bytes} bytes"
+        except ValueError as e:
+            print(e)
 
-    def __convert_size(self, size_bytes):
-        if size_bytes >= 2 ** 30:
-            size_gb = size_bytes / 2 ** 30
-            return f'{round(size_gb, 2)} GB'
-        elif size_bytes >= 2 ** 20:
-            size_mb = size_bytes / 2 ** 20
-            return f'{round(size_mb, 2)} MB'
-        elif size_bytes >= 2 ** 10:
-            size_kb = size_bytes / 2 ** 10
-            return f'{round(size_kb, 2)} KB'
-        else:
-            return f'{size_bytes} bytes'
+    def _get_folder_size(self, path: str) -> str:
+        try:
+            if not os.path.isdir(path):
+                raise ValueError("Error")
+            size_bytes = sum(
+                os.path.getsize(os.path.join(dirpath, filename)) for dirpath, _, filenames in os.walk(path) for filename
+                in filenames)
+            return self._convert_size(size_bytes)
+        except ValueError as e:
+            print(e)
 
     def run(self, path: str):
         print('Calculating the bytes ...')
-        folder_size = self.__get_size(path)
-        self.__convert_size(folder_size)
-        print(f'{path}: {folder_size} bytes')
+        folder_size = self._get_folder_size(path)
+        print(folder_size)
