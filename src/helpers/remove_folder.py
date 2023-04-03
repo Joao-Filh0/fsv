331389de913f3379a2f93d2063e1f73c30a9e718
joao <joaoadmob2@gmail.com>
2023-04-03 00:21:55 -0300

    delete win+2

--- src/helpers/remove_folder.py
+++ src/helpers/remove_folder.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 import shutil
 import os
+from src.helpers.validate_platform import is_win
 
 
 def remove_folder(dir_path):
+    print("Deleting ...")
     for item in os.listdir(dir_path):
         item_path = os.path.join(dir_path, item)
 
         if os.path.isfile(item_path):
             os.unlink(item_path)
         else:
-            is_win = is_win()
-            if is_win:
+            win = is_win()
+            if win:
                 os.chmod(item_path, 0o700)
                 os.system(f'rmdir /s /q "{item_path}"')
             else:
